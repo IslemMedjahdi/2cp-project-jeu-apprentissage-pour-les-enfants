5b7c69e41fec29ec6a43ed7618dc389b59c25fd3
Habouche khaled Abderrahmène <kk_habouche@esi.dz>
2022-05-21 21:14:14 +0200

    adding intros

---
+++ app/screens/IntroTheme.js
@@ -0,0 +1,138 @@
+import {
+  ImageBackground,
+  Image,
+  Pressable,
+  StatusBar,
+  Text,
+  View,
+  BackHandler,
+  Dimensions,
+} from "react-native";
+import { useSelector } from "react-redux";
+import colors from "../data/colors";
+import React from "react";
+import { useEffect } from "react";
+import * as Animatable from "react-native-animatable";
+import themes from "../data/themes";
+import { InputRightAddon } from "native-base";
+
+export default function IntroTheme({ navigation, route }) {
+  const { index } = route.params;
+  const user = useSelector((state) => state.user.value);
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      () => {
+        return true;
+      }
+    );
+    return () => backHandler.remove();
+  }, []);
+  return (
+    <View>
+      <StatusBar
+        backgroundColor={"black"}
+        translucent={true}
+        barStyle={"light-content"}
+      />
+      <ImageBackground
+        source={themes[index].backgroundGame}
+        resizeMode="cover"
+        style={{
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Animatable.View
+          animation={"zoomIn"}
+          duration={1000}
+          style={{
+            width: "85%",
+            backgroundColor: "#FFFFFFDD",
+            padding: 5,
+            borderRadius: 20,
+            height: (50 * Dimensions.get("window").height) / 100,
+          }}
+        >
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Animatable.View
+              delay={500}
+              duration={1000}
+              animation={"fadeInLeft"}
+              style={{ height: (40 * Dimensions.get("window").height) / 100 }}
+            >
+              <Text
+                style={{
+                  fontFamily: user.language === 2 ? "ArbFont" : "RowdiesBold",
+                  fontSize: user.language === 2 ? 30 : 26,
+                }}
+              >
+                {themes[index].intro[user.language]}
+              </Text>
+            </Animatable.View>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent : "center",
+              width : "100%",
+              height: (10 * Dimensions.get("window").height) / 100,
+            }}
+          >
+            <Pressable
+              onPress={() => {
+                navigation.replace("QuizzTest", { index });
+              }}
+            >
+              {({ pressed }) => (
+                <Text
+                  style={{
+                    color: pressed ? colors.MAIN + "80" : colors.MAIN,
+                    fontFamily: user.language === 2 ? "ArbFont" : "RowdiesBold",
+                    fontSize: user.language === 2 ? 22 : 20,
+                    textAlign: "center",
+                  }}
+                >
+                  {user.language === 0
+                    ? "Cliquer"
+                    : user.language === 1
+                    ? "Click"
+                    : "إضغط"}
+                </Text>
+              )}
+            </Pressable>
+            <Animatable.View
+              animation={"swing"}
+              duration={2000}
+              iterationCount="infinite"
+              style={{
+                width: 130,
+                position : "relative",
+                bottom : 100,
+                left : 50
+              }}
+            >
+              <Image
+                resizeMode="contain"
+                style={{
+                  transform: [
+                    { rotateY: user.language === 2 ? "0deg" : "180deg" },
+                  ],
+                  width: "100%",
+                }}
+                source={require("../../assets/hero/mystick0.png")}
+              />
+            </Animatable.View>
+          </View>
+        </Animatable.View>
+      </ImageBackground>
+    </View>
+  );
+}
--- app/screens/Story.js
+++ app/screens/Story.js
@@ -1,153 +1,143 @@
-import { ResizeMode } from "expo-av";
 import React, { useState } from "react";
 
-import { ImageBackground, StyleSheet, Text, View,Image } from "react-native";
-import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
-import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
-import { Colors } from "react-native/Libraries/NewAppScreen";
-import colors from "../data/colors";
+import { ImageBackground, Pressable, Text, View, Image } from "react-native";
 
-export default function Story({navigation}){
-  const [page,setPage] = useState(0);
-return (
-  <View style={{flex: 1}}>
-    
-    <ImageBackground source={require("../../assets/background/background0.jpg")} resizeMode="cover"
-                     style={{ flex: 1,
-                             // justifyContent: "center"
-                               }}>
-        <View style={{ flex:1.05,
-                      justifyContent:"flex-end",
-                     // backgroundColor:"black",
-                       
-        }}>
-          <Text  style={{fontFamily:  "RowdiesBold",
-                        textAlign: "center",
-                         fontSize:30,
-                         color:"#ffffff",
-                         
-                        }}>MON HISTOIRE</Text>
-        </View>
-        <View style={{flex:2.5,
-                     // margin:18,
-                     marginTop:40,
-                      marginLeft:18,
-                      marginRight:18,
-                      borderRadius:50,
-                      backgroundColor:'rgba(255,255,255,0.8)',
-           }}>
-           <View style={{flex:2.5,
-                          marginTop:40,
-                          opacity:1,
-          }}>
-           <Text style={{fontFamily:  "RowdiesBold",
-                        textAlign: "center",
-                         fontSize:27,
-                         color:"#000000",
-                         }}>
-                   {page===4 
-                   ? "Aujourd'hui je me sens en bonne santé et je suis là pour t'éviter de tomber dans le même piège ... "
-                   : page===3
-                   ? "Mais le jour où j'ai commencé à me sentir mal dans ma peau, j'ai décidé de faire attention à mon alimentation ..."
-                   : page ===2
-                   ? "Avec le temps j'ai commencé à grossir, de jour en jour je suis devenu obèse ..."
-                   : page===1
-                   ?"Mon estomac me faisait souvent mal et je me retrouvais malade d'intoxication ..."
-                   :"Auparavant je ne faisais pas attention à mon alimentation, je mangeais tout et n'importe quoi ..."
-                  }
-                  </Text>
-          </View>
-          <View style={{flex:2,
-                         justifyContent:"center",
-                         alignItems:"center",
-                         opacity:1,
-          }}>
-            {page===0
-             ? <Image 
-                  
-                  resizeMode="contain"
-                  style={{height:177,
-                          width: 211,
-                          marginBottom:40,
-                  }}
-                  source={require("../../assets/hero/mystick2.png")}        
-          />
-          : page ===1
-          ?<Image 
-                  
-            resizeMode="contain"
-            style={{height:177,
-                    width: 211,
-                    marginBottom:40,
-            }}
-            source={require("../../assets/hero/mystick15.webp")}      
-         />
-          : page===2
-          ? <Image 
-                  
-            resizeMode="contain"
-            style={{height:177,
-                    width: 211,
-                    marginBottom:40,
-            }}
-             source={require("../../assets/hero/mystick14.webp")}      
-           />
-          :page===3
-          ?<Image       
-                  resizeMode="contain"
-                  style={{height:177,
-                          width: 211,
-                          marginBottom:40,
-                  }}
-                  source={require("../../assets/hero/mystick13.webp")}      
-          />
-         :<Image  resizeMode="contain"
-                  style={{height:177,
-                          width: 211,
-                          marginBottom:40,
-                  }}
-                  source={require("../../assets/hero/mystick16.webp")}      
-          />
-           }
-          </View>
-        </View>
-        
-        <View style={{flex:1.3,
-                       alignItems: page===4?"center":"flex-end",
-                      //justifyContent:"flex-end",
-                       justifyContent: page===4 ? "center" : "flex-end",
-                      //alignItems:"flex-end",
-                      marginRight:30,
-        }}>
-         <Pressable
-                    onPress={() => {page===4? navigation.replace("SelectTheme"):setPage(page+1)}}
-                    android_ripple={{ color: "#ffffff30" ,  
-                  }}
-          >
-            
-            { page===4
-            ?<Text
+
+export default function Story({ navigation }) {
+  const [page, setPage] = useState(0);
+  return (
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        source={require("../../assets/background/background0.jpg")}
+        resizeMode="cover"
+        style={{
+          flex: 1,
+        }}
+      >
+        <View
+          style={{
+            flex: 1.05,
+            justifyContent: "flex-end",
+          }}
+        >
+          <Text
             style={{
-             color:"white",
               fontFamily: "RowdiesBold",
-              fontSize:  30,
               textAlign: "center",
+              fontSize: 30,
+              color: "#ffffff",
             }}
           >
-            Cliquez
+            MON HISTOIRE
           </Text>
-            : <Image source={require("../../assets/icons/continuer2.png")}></Image>
-             }
-      </Pressable>
+        </View>
+        <View
+          style={{
+            flex: 2.5,
+            marginTop: 40,
+            marginLeft: 18,
+            marginRight: 18,
+            borderRadius: 50,
+            backgroundColor: "rgba(255,255,255,0.8)",
+          }}
+        >
+          <View style={{ flex: 2.5, marginTop: 40, opacity: 1 }}>
+            <Text
+              style={{
+                fontFamily: "RowdiesBold",
+                textAlign: "center",
+                fontSize: 27,
+                color: "#000000",
+              }}
+            >
+              {page === 4
+                ? "Aujourd'hui je me sens en bonne santé et je suis là pour t'éviter de tomber dans le même piège ... "
+                : page === 3
+                ? "Mais le jour où j'ai commencé à me sentir mal dans ma peau, j'ai décidé de faire attention à mon alimentation ..."
+                : page === 2
+                ? "Avec le temps j'ai commencé à grossir, de jour en jour je suis devenu obèse ..."
+                : page === 1
+                ? "Mon estomac me faisait souvent mal et je me retrouvais malade d'intoxication ..."
+                : "Auparavant je ne faisais pas attention à mon alimentation, je mangeais tout et n'importe quoi ..."}
+            </Text>
+          </View>
+          <View
+            style={{
+              flex: 2,
+              justifyContent: "center",
+              alignItems: "center",
+              opacity: 1,
+            }}
+          >
+            {page === 0 ? (
+              <Image
+                resizeMode="contain"
+                style={{ height: 177, width: 211, marginBottom: 40 }}
+                source={require("../../assets/hero/mystick2.png")}
+              />
+            ) : page === 1 ? (
+              <Image
+                resizeMode="contain"
+                style={{ height: 177, width: 211, marginBottom: 40 }}
+                source={require("../../assets/hero/mystick15.webp")}
+              />
+            ) : page === 2 ? (
+              <Image
+                resizeMode="contain"
+                style={{ height: 177, width: 211, marginBottom: 40 }}
+                source={require("../../assets/hero/mystick14.webp")}
+              />
+            ) : page === 3 ? (
+              <Image
+                resizeMode="contain"
+                style={{ height: 177, width: 211, marginBottom: 40 }}
+                source={require("../../assets/hero/mystick13.webp")}
+              />
+            ) : (
+              <Image
+                resizeMode="contain"
+                style={{ height: 177, width: 211, marginBottom: 40 }}
+                source={require("../../assets/hero/mystick16.webp")}
+              />
+            )}
+          </View>
         </View>
 
-                                
-     </ ImageBackground>                           
-                                                   
-  </View>
-  
-);
+        <View
+          style={{
+            flex: 1.3,
+            alignItems: page === 4 ? "center" : "flex-end",
+            justifyContent: page === 4 ? "center" : "flex-end",
+            marginRight: 30,
+          }}
+        >
+          <Pressable
+            onPress={() => {
+              page === 4
+                ? navigation.replace("SelectTheme")
+                : setPage(page + 1);
+            }}
+            android_ripple={{ color: "#ffffff30" }}
+          >
+            {page === 4 ? (
+              <Text
+                style={{
+                  color: "white",
+                  fontFamily: "RowdiesBold",
+                  fontSize: 30,
+                  textAlign: "center",
+                }}
+              >
+                Cliquez
+              </Text>
+            ) : (
+              <Image
+                source={require("../../assets/icons/continuer2.png")}
+              ></Image>
+            )}
+          </Pressable>
+        </View>
+      </ImageBackground>
+    </View>
+  );
 }
-
-
-
